bf4ff4650469e4caa277640227d85cb50ea8815a
Jennie Oliver <jennie.king@gmail.com>
2019-10-27 08:57:24 -0500

    init

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'blahblahblah',
+            'author': 'Jennie Oliver',
+            'create_date': '10-13-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'blahblahblah2',
+            'author': 'Benji Dog',
+            'create_date': '09-13-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'blahblahblah',
+            'author': 'Jennie Oliver',
+            'create_date': '08-13-2019'
+        }
+    ]
+    return articles
